765df1ca108b219d93d4eee72e26e6d1e3844e3b
Milosz Danilczuk <md321825@apc9.okwf.fuw.edu.pl>
2013-05-28 11:10:36 +0200

    w repo

---
+++ void.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Milosz Danilczuk
+import numpy as np
+import pylab as py
+import scipy.io as sio
+import scipy.stats as st
+import numpy.random as rnd
+import random
+from collections import *
+import math
+
